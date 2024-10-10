4a40b72eb068120630012de2847aeeff9a779133
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-10-11 01:23:48 +0600

    all categories added

--- scripts/meal.js
+++ scripts/meal.js
@@ -1,1 +1,32 @@
 console.log('connected');
+
+const loadCategories = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then((response) => response.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((err) => console.log(err));
+};
+
+const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById('categories');
+    categories.forEach((item) => {
+        // console.log(item.strCategory)
+        const div = document.createElement('div');
+        div.classList = 'card lg:card-side bg-base-100 shadow-xl'
+        div.innerHTML = `
+        <figure>
+            <img class="w-full" src="${item.strCategoryThumb}" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${item.strCategory}</h2>
+            <p>${item.strCategoryDescription.slice(0,100)}</p>
+            <div class="card-actions justify-start">
+                <button class="text-primary font-bold underline">View Details</button>
+            </div>
+        </div>
+        `
+        categoriesContainer.append(div);
+    })
+}
+
+loadCategories();
